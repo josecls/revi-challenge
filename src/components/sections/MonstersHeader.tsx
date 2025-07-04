e09116ce9fc520e5f083ed12ad85b953ad688b31
josecls <josecls.dev@gmail.com>
2025-07-04 10:20:49 -0300

    fix(monsters list): changed top spacing and did some refactoring to keep code organized

---
+++ src/components/sections/MonstersHeader.tsx
@@ -0,0 +1,93 @@
+import { PlusCircleIcon } from "lucide-react";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { useMonsters } from "@/contexts/MonsterContext";
+import { useState } from "react";
+import { toast } from "sonner";
+
+function MonstersHeader() {
+    const [monster, setMonster] = useState({
+        name: '',
+        attack: 0,
+        defense: 0,
+        speed: 0,
+        hp: 0,
+        image_url: '',
+      });
+    const { addMonster } = useMonsters();
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    
+    console.log(monster)
+    if (!monster.name || !monster.image_url) {
+      toast("Please fill in all fields.");
+      return;
+    }
+
+    if (monster.attack < 0 || monster.defense < 0 || monster.speed < 0 || monster.hp < 0) {
+      toast("Attack, Defense, Speed, and HP must be non-negative.");
+      return;
+    }
+
+    if (monster.attack === 0 && monster.defense === 0 && monster.speed === 0 && monster.hp === 0) {
+      toast("Monster attributes cannot all be zero.");
+      return;
+    }
+
+    addMonster(monster);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setMonster((prevMonster) => ({
+      ...prevMonster,
+      [name]: name === 'attack' || name === 'defense' || name === 'speed' || name === 'hp' ? parseInt(value, 10) : value,
+    }));
+  }
+  return (
+    <div className="flex flex-col-reverse md:flex-row justify-center md:justify-between items-center px-4 md:px-12 lg:px-20 xl:px-32 mb-8 mt-50">
+      <h1 className="text-4xl md:text-5xl font-extrabold font-[fredoka] text-[#674AA3] mt-10 md:mt-0">Monsters List</h1>
+      <Dialog>
+        <DialogTrigger className="flex justiy-center align-center bg-[#1EBA1E] font-[fredoka] text-white text-xl px-7 py-4 rounded-xl transition-transform duration-300 transform hover:scale-105 cursor-pointer">
+            <PlusCircleIcon className="size-6 mr-2" />
+            New Monster
+        </DialogTrigger>
+        <DialogContent className="bg-white">
+          <DialogHeader className="text-[#674AA3]">
+            <DialogTitle className="text-2xl">Create a new monster</DialogTitle>
+          </DialogHeader>
+          <form className="flex flex-col gap-1" onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <Label className="text-sm mb-2 font-bold">Monster Name</Label>
+              <Input name="name" onChange={handleInputChange} placeholder="e.g. Alfred" type="text"></Input>
+            </div>
+            <div className="mb-3">
+              <Label className="text-sm mb-2 font-bold">Attack</Label>
+              <Input name="attack" onChange={handleInputChange} placeholder="e.g. 10" type="number"></Input>
+            </div>
+            <div className="mb-3">
+              <Label className="text-sm mb-2 font-bold">Defense</Label>
+              <Input name="defense" onChange={handleInputChange} placeholder="e.g. 4" type="number"></Input>
+            </div>
+            <div className="mb-3">
+              <Label className="text-sm mb-2 font-bold">Speed</Label>
+              <Input name="speed" onChange={handleInputChange} placeholder="e.g. 3" type="number"></Input>
+            </div>
+            <div className="mb-3">
+              <Label className="text-sm mb-2 font-bold">HP</Label>
+              <Input name="hp" onChange={handleInputChange} placeholder="e.g. 50" type="number"></Input>
+            </div>
+            <div className="mb-3">
+              <Label className="text-sm mb-2 font-bold">Image URL</Label>
+              <Input name="image_url" onChange={handleInputChange} placeholder="e.g. https://example.com/image.jpg" type="url"></Input>
+            </div>
+            <Input type="submit" value="Create Monster" className="mt-4 bg-[#1EBA1E] text-white font-bold py-2 px-4 rounded-lg cursor-pointer hover:bg-green-600 transition-colors duration-300" />
+          </form>
+        </DialogContent>
+      </Dialog>
+    </div>
+  )
+}
+
+export default MonstersHeader;
--- src/pages/Monsters.tsx
+++ src/pages/Monsters.tsx
@@ -1,12 +1,11 @@
 import MonstersList from "@/components/sections/MonstersList";
-
+import MonstersHeader from "@/components/sections/MonstersHeader";
 
 export default function Monsters() {
     return (
-        <div>
-            <div /> {/* Top spacer */}
-
-            <main className="">
+        <div className="w-full">
+            <main className="w-full">
+                <MonstersHeader />
                 <MonstersList />
             </main>
         </div>
